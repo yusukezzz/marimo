fd4f2ebdf7f80c2451bda58c9fb03642fe2b4521
yusukezzz <g0632198@gmail.com>
2013-07-30 00:03:13 +0900

    fix verbose trim

--- rules/athome.js
+++ rules/athome.js
@@ -3,19 +3,19 @@ exports.replyPattern = /https?:\/\/www\.athome\.co\.jp\/([a-z0-9\-.]+)[^\s]+/;
 exports.reply = function(client, nick, to, dom)
 {
   var items = [];
-  items.push(dom('.price').text().trim());
-  items.push(dom('.upkeep').text().trim());
-  items.push(dom('.reward').text().trim());
-  var tmp = dom('.deposit').text().trim().split('/');
-  items.push(tmp[0].trim());
-  items.push(dom('.layout').text().trim());
-  items.push(dom('.area').text().trim());
-  items.push(dom('.type').text().trim().replace('賃貸', ''));
-  items.push(dom('.completion').text().trim());
+  items.push(dom('.price').text());
+  items.push(dom('.upkeep').text());
+  items.push(dom('.reward').text());
+  var tmp = dom('.deposit').text().split('/');
+  items.push(tmp[0]);
+  items.push(dom('.layout').text());
+  items.push(dom('.area').text());
+  items.push(dom('.type').text().replace('賃貸', ''));
+  items.push(dom('.completion').text());
   var contents = ['賃', '共', '礼', '敷'];
   for (var i=0; i<4; i++) {
-    items[i] = contents[i] + items[i];
+    items[i] = contents[i] + items[i].trim();
   }
-  var result = items.join(' ');
+  var result = items.map(function(txt){return txt.trim();}).join(' ');
   client.notice(to, result);
 }
--- rules/homes.js
+++ rules/homes.js
@@ -3,22 +3,22 @@ exports.replyPattern = /https?:\/\/www\.homes\.co\.jp\/([a-z0-9\-.]+)[^\s]+/;
 exports.reply = function(client, nick, to, dom)
 {
   var items = [];
-  items.push(dom('dd#chk-bkc-moneyroom > span').text().trim());
-  var tmp = dom('dd#chk-bkc-moneyroom').text().trim().match(/\((.*?)\)/);
+  items.push(dom('dd#chk-bkc-moneyroom > span').text());
+  var tmp = dom('dd#chk-bkc-moneyroom').text().match(/\((.*?)\)/);
   items.push(tmp[1].trim());
-  tmp = dom('dd#chk-bkc-moneyshikirei').text().trim().split('/');
-  items.push(tmp[1].trim());
-  items.push(tmp[0].trim());
-  tmp = dom('dd#chk-bkc-marodi').text().trim().split('\n');
-  items.push(tmp[0].trim());
-  items.push(dom('dd#chk-bkc-housearea').text().trim());
-  items.push(dom('span#chk-bkh-type').text().trim().replace('賃貸', ''));
-  tmp = dom('dd#chk-bkc-kenchikudate').text().trim().match(/\((.*?)\)/);
-  items.push(tmp[1].trim());
+  tmp = dom('dd#chk-bkc-moneyshikirei').text().split('/');
+  items.push(tmp[1]);
+  items.push(tmp[0]);
+  tmp = dom('dd#chk-bkc-marodi').text().split('\n');
+  items.push(tmp[0]);
+  items.push(dom('dd#chk-bkc-housearea').text());
+  items.push(dom('span#chk-bkh-type').text().replace('賃貸', ''));
+  tmp = dom('dd#chk-bkc-kenchikudate').text().match(/\((.*?)\)/);
+  items.push(tmp[1]);
   var contents = ['賃', '共', '礼', '敷'];
   for (var i=0; i<4; i++) {
-    items[i] = contents[i] + items[i];
+    items[i] = contents[i] + items[i].trim();
   }
-  var result = items.join(' ');
+  var result = items.map(function(txt){return txt.trim();}).join(' ');
   client.notice(to, result);
 }
